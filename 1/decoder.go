7238244d0bfb3164a676c843e30ea83cfdee6cb8
Jason Clark <jason@jjcconsultingllc.com>
2015-03-11 16:43:51 -0700

    starting on file read

--- 1/decoder.go
+++ 1/decoder.go
@@ -1,8 +1,15 @@
 package drum
 
-import "errors"
+import (
+	"bytes"
+	"encoding/binary"
+	"errors"
+	"io"
+	"os"
+)
 
 const (
+	errorHeader  = "Input file is not a splice file"
 	errorVersion = "There is an error reading the version identifier"
 	errorTempo   = "There is an error reading the tempo value"
 	errorTrack   = "There is an error reading the track data"
@@ -13,37 +20,55 @@ const (
 // rest of the data.
 // TODO: implement
 func DecodeFile(path string) (*Pattern, error) {
-	// open file for reading
-	// extract file header
-	// extract version
-	// extract tempo
-	// extract tracks
-	var pat Pattern
+	var (
+		pat       Pattern
+		inputFile *os.File
+		err       error
+	)
 
-	input := []byte{0}
-	if err := decodeVersion(input, &pat.version); err != nil {
-		return nil, errors.New(errorVersion)
+	if inputFile, err = os.Open(path); err != nil {
+		return nil, err
 	}
-	if err := decodeTempo(input, &pat.tempo); err != nil {
-		return nil, errors.New(errorTempo)
+	defer inputFile.Close() // Close when function exits
+	if err = decodeHeader(inputFile); err != nil {
+		return nil, err
 	}
-	if err := decodeTracks(input, &pat.tracks); err != nil {
-		return nil, errors.New(errorTrack)
+	if err = decodeVersion(inputFile, &pat.version); err != nil {
+		return nil, err
+	}
+	if err = decodeTempo(inputFile, &pat.tempo); err != nil {
+		return nil, err
+	}
+	if err = decodeTracks(inputFile, &pat.tracks); err != nil {
+		return nil, err
 	}
 	return &pat, nil
 }
 
-func decodeVersion(input []byte, version *string) error {
+var spliceHeader = []byte{0x53, 0x50, 0x4c, 0x49, 0x43, 0x45, 0, 0, 0, 0, 0, 0}
+
+func decodeHeader(input io.Reader) error {
+	header := make([]byte, len(spliceHeader))
+	if err := binary.Read(input, binary.LittleEndian, &header); err != nil {
+		return errors.New(errorHeader)
+	}
+	if !bytes.Equal(header[0:], spliceHeader) {
+		return errors.New(errorHeader)
+	}
+	return nil
+}
+
+func decodeVersion(input io.Reader, version *string) error {
 	*version = "0.808-alpha"
 	return nil
 }
 
-func decodeTempo(input []byte, tempo *float64) error {
+func decodeTempo(input io.Reader, tempo *float64) error {
 	*tempo = 120.0
 	return nil
 }
 
-func decodeTracks(input []byte, tracks *[]Track) error {
+func decodeTracks(input io.Reader, tracks *[]Track) error {
 	*tracks = []Track{
 		{id: 0, name: "kick", steps: [16]bool{true, false, false, false, true, false, false, false, true, false, false, false, true, false, false, false}},
 		{id: 1, name: "snare", steps: [16]bool{false, false, false, false, true, false, false, false, false, false, false, false, true, false, false, false}},
